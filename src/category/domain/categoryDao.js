82faee70a0d50a74c31135a6f45be9f30b0f9e5e
soyoung <seungmochi@gmail.com>
2023-02-02 12:15:14 +0900

    feat: 카테고리 추가 기능 구현 완료
    카테고리 추가 api 작성 완료 및 테스트 예정

--- src/category/domain/categoryDao.js
+++ src/category/domain/categoryDao.js
@@ -4,7 +4,14 @@ const categoryDao = {
   async findAll() {
     return await Category.find({});
   },
+  async create({ name, productCount, description }){
+    const category = new Category ({ name, productCount, description }); 
+    await category.save(); 
+    return category.toObject(); 
+  },
 };
+
+
 
 module.exports = categoryDao;
 
