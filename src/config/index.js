8ce37279672f288c31e3c3113d1e641b6f374700
BrianDYKim <18k7102dy@naver.com>
2023-02-07 22:30:33 +0900

    fix: cors 적용

--- src/config/index.js
+++ src/config/index.js
@@ -3,14 +3,6 @@ const dotenv = require('dotenv');
 const { AppError } = require('../misc/AppError');
 const { commonErrors } = require('../misc/commonErrors');
 
-// Node Environment => 노드가 지금 production 환경? development 환경인가?
-process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
-console.log(
-  `어플리케이션을 다음의 환경으로 시작합니다: ${process.env.NODE_ENV}`
-);
-
-process.env.PORT = process.env.PORT ?? 3500;
-
 // .env 파일이 정의되어있는지?
 const envFound = dotenv.config();
 if (envFound.error) {
@@ -21,6 +13,14 @@ if (envFound.error) {
   );
 }
 
+process.env.PORT = process.env.PORT ?? 3500;
+
+// Node Environment => 노드가 지금 production 환경? development 환경인가?
+process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
+console.log(
+  `어플리케이션을 다음의 환경으로 시작합니다: ${process.env.NODE_ENV}`
+);
+
 process.env.AUTH_SERVER_URL = process.env.AUTH_SERVER_URL ?? 'http://localhost:4000/api/users'
 
 // mongoDBUrl을 config에서 정의
@@ -29,6 +29,7 @@ const mongoDbUrl = process.env.MONGODB_URL ?? 'mongodb://localhost:27017';
 module.exports = {
   applicationName: process.env.APPLICATION_NAME || 'app',
   port: process.env.PORT,
+  environment: process.env.NODE_ENV, 
   mongoDbUrl: mongoDbUrl,
   authServerUrl: process.env.AUTH_SERVER_URL
 };
